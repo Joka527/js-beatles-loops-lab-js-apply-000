a33f50828d468d73ea2d571bb93926cf43c5b504
John K <joka527@gmail.com>
2018-05-19 06:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function theBeatlesPlay(musicians, instruments){
   let sentences=[];
   for(let i=0;musicians.length>i; i++){
+    let sentence="";
+    sentence+="plays";
+    for(let i=0;instruments.length>i; i++){
+       
+    }
     
+    setences.push();
   }
   return sentences;
 }
